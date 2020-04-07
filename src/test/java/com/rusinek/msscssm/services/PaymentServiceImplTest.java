58fa5096e73b47385d4ebf3f7975d6b49b40a505
AdRusinek <rusinekadrian19@gmail.com>
2020-04-07 04:48:10 +0200

    taking care og auth action

--- src/test/java/com/rusinek/msscssm/services/PaymentServiceImplTest.java
+++ src/test/java/com/rusinek/msscssm/services/PaymentServiceImplTest.java
@@ -5,6 +5,7 @@ import com.rusinek.msscssm.domain.PaymentEvent;
 import com.rusinek.msscssm.domain.PaymentState;
 import com.rusinek.msscssm.repository.PaymentRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -47,5 +48,23 @@ class PaymentServiceImplTest {
         System.out.println(sm.getState().getId());
 
         System.out.println(preAuthPayment);
+    }
+
+    @Transactional
+    @RepeatedTest(10)
+    void Auth() {
+        Payment savedPayment = paymentService.newPayment(this.payment);
+
+        StateMachine<PaymentState, PaymentEvent> preAuthSM = paymentService.preAuth(savedPayment.getId());
+
+        if (preAuthSM.getState().getId() == PaymentState.PRE_AUTH) {
+            System.out.println("Payment is pre authorized");
+
+            StateMachine<PaymentState, PaymentEvent> authSM = paymentService.authorizePayment(savedPayment.getId());
+
+            System.out.println("result of auth: " + authSM.getState().getId());
+        } else {
+            System.out.println("Payment failed pre auth");
+        }
     }
 }
